97145ed0acb31a0c3308e25bcc99c079f2eb2e4f
DeveloperrEva <lemon.mgdn@icloud.com>
2023-01-05 15:54:35 +0300

    add

---
+++ vpn_bot/misc/config.py
@@ -0,0 +1,8 @@
+bot_token="5820823115:AAEmmsYcSRe_1uyQEp_RwOLhmV7Pmaem3I4"
+
+lolz_api=["c2c02c682c2e0676f86b0a7c02006501f46213f8", "209087", "developereva"]
+crystal_pay=["Имя кассы(Crystal Pay)", "Токен кассы"]
+qiwi_key="Ключ киви"
+
+lolz_theme="zelenka.guru/developereva"
+group_id="5258776765"
