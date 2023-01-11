7a4f647ddca427dcf94a1a69d626eefb5cb0867f
DeveloperrEva <35002804+DeveloperrEva@users.noreply.github.com>
2023-01-11 10:52:03 +0300

    Update config.py

--- vpn_bot/misc/config.py
+++ vpn_bot/misc/config.py
@@ -1,8 +1,8 @@
-bot_token="5820823115:AAEmmsYcSRe_1uyQEp_RwOLhmV7Pmaem3I4"
+bot_token=""
 
-lolz_api=["c2c02c682c2e0676f86b0a7c02006501f46213f8", "209087", "developereva"]
+lolz_api=["", "", ""]
 crystal_pay=["Имя кассы(Crystal Pay)", "Токен кассы"]
-qiwi_key="59fb7a63111f898c80ebc0618901ca1d"
+qiwi_key=""
 
-lolz_theme="zelenka.guru/developereva"
-group_id="5258776765"
+lolz_theme=""
+group_id=""
